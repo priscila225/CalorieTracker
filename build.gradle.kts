785d9da0115efab533d2b89bbd3dd5d3953e3ab6
Priscila Apocalypse <pri.apocalypse@gmail.com>
2022-12-08 17:34:06 -0300

    Migrating to Gradle Kotlin DSL

---
+++ build.gradle.kts
@@ -0,0 +1,16 @@
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+    }
+    dependencies {
+        classpath(Build.androidBuildTools)
+        classpath(Build.hiltAndroidGradlePlugin)
+        classpath(Build.kotlinGradlePlugin)
+    }
+}
+
+tasks.register("clean", Delete::class) {
+    delete(rootProject.buildDir)
+}
---
+++ buildSrc/src/main/kotlin/Modules.kt
@@ -0,0 +1,13 @@
+object Modules {
+    const val app = ":app"
+
+    const val core = ":core"
+    const val coreUi = ":core-ui"
+
+    const val onboardingDomain = ":feature-onboarding:onboarding_domain"
+    const val onboardingPresentation = ":feature-onboarding:onboarding_presentation"
+
+    const val trackerData = ":tracker:tracker_data"
+    const val trackerDomain = ":tracker:tracker_domain"
+    const val trackerPresentation = ":tracker:tracker_presentation"
+}
